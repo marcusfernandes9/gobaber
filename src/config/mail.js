00e6cd2829fc736eb88b76ab2f60ec39d9fd7d24
-GitName mfernandes <-GitEmail mfernandes@esss.co>
2019-11-22 14:36:21 -0300

    Configurando templates de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '876c306e510c87',
+    pass: 'e569bab1f4e74c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
